99e1beb52523cb6df4d3911876cd428897e75774
boxing-in-iT <tbol240@gmail.com>
2024-06-27 00:42:31 +0300

    test

--- src/components/welcome/index.jsx
+++ src/components/welcome/index.jsx
@@ -59,10 +59,7 @@ const Title = styled.h1`
     font-size: 80px;
     font-weight: 900;
     margin-bottom: 0; /* Adjusted margin for smaller screens */
-    text-shadow: 0 0 5px rgba(0, 255, 0, 0.5), 0 0 5px rgba(0, 255, 0, 0.5),
-      0 0 10px rgba(0, 255, 0, 0.5), 0 0 10px rgba(0, 255, 0, 0.5),
-      0 0 15px rgba(0, 255, 0, 0.5), 0 0 15px rgba(0, 255, 0, 0.5),
-      0 0 15px rgba(0, 255, 0, 0.5); /* Adjusted for smaller screens */
+    text-shadow: 0 0 10px rgba(0, 255, 0, 0.5);
   }
 `;
 
